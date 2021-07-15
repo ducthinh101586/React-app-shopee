31c509a1f668b515b01b4d2cbfb22a6859a553b3
DucThinh <ngoducthinh586@gmail.com>
2021-07-15 17:32:58 +0700

    sửa file ErrorMessage

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Message } from './errorMessage.style'
-import { PropsTypes } from 'prop-types'
-
+import PropTypes from 'prop-types'
 export default function ErrorMessage({ errors, name }) {
   const error = errors[name]
   return <Message>{error && error.message}</Message>
 }
 
 ErrorMessage.propTypes = {
-  errors: PropsTypes.object,
-  name: PropsTypes.string
+  errors: PropTypes.object,
+  name: PropTypes.string
 }
--- src/components/ErrorMessage/errorMessage.style.js
+++ src/components/ErrorMessage/errorMessage.style.js
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 export const Message = styled.div`
   color: #ff424f;
   font-size: 1.2rem;
-  paddingtop: 0.5rem;
+  padding-top: 0.5rem;
 `
